2f27d1cecae9b58611dcbf882a3ea0951101133b
ABHISHEK-VM <abhishekvm819@gmail.com>
2022-07-28 18:56:46 +0530

    table page done

--- table.js
+++ table.js
@@ -1,14 +1,49 @@
-$(function () {
-    $('#datetimepicker1').datetimepicker({
-        format: 'DD-MM-YYYY LT'
+
+
+
+
+$(document).ready(function(){
+
+    
+
+
+  
+    
+    $('img.seats').on('click', function() {
+        
+        var x  = $(this).prop("src");
+        let status = x.slice(-7,-6);
+        let seatNo = x.slice(-6,-4);
+        console.log(x);
+        console.log(status);
+        console.log(seatNo);
+        
+        if (status == "s") {
+            $(this).prop("src", "img/c" + seatNo + ".png");  
+        } else if ( status == "c") {
+            $(this).prop("src", "img/s" + seatNo + ".png");
+        }
     });
-    $('#datetimepicker2').datetimepicker({
-        format: 'DD-MM-YYYY'
-    });
-    $('#datetimepicker3').datetimepicker({
-        format: 'LT'
-    });
-    $('#datetimepicker3').datetimepicker({
-        format: 'LT'
-    });
+
+    
 });
+
+
+
+
+
+// $(function () {
+//     $('#datetimepicker1').datetimepicker({
+//         format: 'DD-MM-YYYY LT'
+//     });
+//     $('#datetimepicker2').datetimepicker({
+//         format: 'DD-MM-YYYY'
+//     });
+//     $('#datetimepicker3').datetimepicker({
+//         format: 'LT'
+//     });
+//     $('#datetimepicker3').datetimepicker({
+//         format: 'LT'
+//     });
+// });
+
